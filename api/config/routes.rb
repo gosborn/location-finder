6248a851fd3befcce05de71c10d7103a4b161b2d
Greg Osborn <gosborn@gmail.com>
2020-09-26 23:08:09 -0400

    Scaffolds Location model

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :locations
   get 'profile', action: :show, controller: 'users'
   resources :users, only: %w(show create destroy) do
     collection do
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_26_205445) do
+ActiveRecord::Schema.define(version: 2020_09_27_030413) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "locations", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "latitude", precision: 10, scale: 6
+    t.decimal "longitude", precision: 10, scale: 6
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_locations_on_name", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
